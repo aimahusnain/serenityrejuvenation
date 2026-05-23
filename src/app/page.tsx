514efe7d44cce5c88727c7cc839a077ca8a06411
Muhammad Husnain <aimahusnain@gmail.com>
2026-05-24 00:46:33 +0500

    Changed height to 750

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ const Home = () => {
         src="/hero_pose_mobile.png"
         alt="Hero Mobile"
         width={800}
-        height={900}
+        height={750}
         className="block md:hidden w-screen h-auto object-cover"
         priority
       />
