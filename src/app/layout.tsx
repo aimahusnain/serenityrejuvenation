8a35665a8029e0baf539c45dc8ab37483c5396b4
Muhammad Husnain <aimahusnain@gmail.com>
2026-05-24 15:33:19 +0500

    Added lenis smooth scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,13 @@ import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
 import SpaLoader from "@/components/loader";
+import LenisProvider from "@/components/LenisProvider";
 
-const fontSans = Geist({ subsets: ['latin'], variable: '--font-sans' });
+const fontSans = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Serenity Rejuvenation",
-  description:
-    "Luxury hydration spa focused on restore, rebalance, and renew.",
+  description: "Luxury hydration spa focused on restore, rebalance, and renew.",
 };
 
 export default function RootLayout({
@@ -18,11 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning className={fontSans.variable}>
-      <body
-        className="antialiased bg-[ecf4dd]"
-      >
+      <body className="antialiased bg-[ecf4dd]">
         <SpaLoader />
-        {children}
+        <LenisProvider>{children}</LenisProvider>
       </body>
     </html>
   );
